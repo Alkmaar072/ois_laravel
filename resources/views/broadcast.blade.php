2aea8592f97f333aff5bf7af14b047b09dd55fd7
Thomas Dinkgreve <thomasdinkgreve@gmail.com>
2023-12-06 23:23:26 +0100

    1

---
+++ resources/views/broadcast.blade.php
@@ -0,0 +1,4 @@
+<div class="right message">
+  <p>{{$message}}</p>
+    <img src="/avatar1.jfif" height="45px" alt="Avatar">
+</div>
---
+++ resources/views/receive.blade.php
@@ -0,0 +1,4 @@
+<div class="left message">
+    <img src="/avatar.jfif" height="45px" alt="Avatar">
+  <p>{{$message}}</p>
+</div>
